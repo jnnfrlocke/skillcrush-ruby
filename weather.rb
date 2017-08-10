8edba717e6917c35d16a887081892936a5c50827
Jen <jnnfrlocke@gmail.com>
2017-08-10 18:21:42 -0500

    Refactor weather.rb

--- weather.rb
+++ weather.rb
@@ -1,12 +1,20 @@
 require 'barometer'
 
-# def get_location()
 puts "What's your location?"
-# location = gets.chomp
-# end
 
 barometer = Barometer.new(gets.chomp)
 weather = barometer.measure
+tomorrow = Time.now.strftime('%d').to_i + 1
 
-puts weather.current.condition
+forecast = weather.forecast
+
+forecast.each do |daily| 
+    d = daily.starts_at.day
+    if d == tomorrow
+    dayName = 'Tomorrow'
+  else
+    dayName = daily.starts_at.strftime('%A')
+  end
+    puts "#{dayName}'s high is #{daily.high.f} and it will be #{daily.icon} outside."
+    end
 
