1427fc99914bd9e1fef73a2e7276dbfc7621999b
Jen <jnnfrlocke@gmail.com>
2017-08-10 15:12:23 -0500

    Create nokogiri.rb, scrape_martha.rb, hash_symbols.rb

--- hash_symbols.rb
+++ hash_symbols.rb
@@ -1,9 +1,9 @@
 {
-  :name => "Kelly",
-  :email => "kelly.wonderful@gmail.com"
-  :name => "Sarah",
-  :email => "sth@one.com",
-  :name => "Person",
-  :email => "email@something.com"
+  name: "Kelly",
+  email: "kelly.wonderful@gmail.com"
+  name: "Sarah",
+  email: "sth@one.com",
+  name: "Person",
+  email: "email@something.com"
 }
 
---
+++ scrape_martha.rb
@@ -0,0 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open("http://www.marthastewart.com/312598/brick-pressed-sandwich"))
+
+puts doc.search(".components-item").inner_text
+
