a8e307c78b0fc0f065065bfa2b4533dc909a62e4
Jen <jnnfrlocke@gmail.com>
2017-08-09 19:31:59 -0500

    Fix errors in pets.rb

--- pets.rb
+++ pets.rb
@@ -1,6 +1,6 @@
-class ferret
+class Ferret
  
-	def set_name = (ferret_name)
+	def set_name=(ferret_name)
 		@name = ferret_name
 	end
  
@@ -28,6 +28,10 @@ class Chinchilla
 		@name = chinchilla_name
 	end
  
+def get_name
+		return @name
+	end
+
 	def set_owner=(owner_name)
 		@owner_name = owner_name
 	end
@@ -45,7 +49,7 @@ end
 class Parrot
  
 	def set_name=(parrot_name)
-		@name = name
+		@name = parrot_name
 	end
  
 	def get_name
@@ -58,26 +62,30 @@ class Parrot
  
 	def get_owner
 		return @owner_name
+    end
+    
+    def tweet
+		return "tweeeeet"
 	end
  
 end
  
 my_ferret = Ferret.new
 my_ferret.set_name= "Fredo"
-ferretname = my_ferret.get_name
+ferret_name = my_ferret.get_name
  
 my_parrot = Parrot.new
 my_parrot.set_name= "Budgie"
-parrotname = my_parrot.get_name
+parrot_name = my_parrot.get_name
  
-my_chincilla = Chincilla.new
-my_chincilla.set_name= "Dali"
-chincillaname = my_chincilla.get_name
+my_chinchilla = Chinchilla.new
+my_chinchilla.set_name= "Dali"
+chinchilla_name = my_chinchilla.get_name
  
-puts "#{ferretname} says #{my_ferret.squeal}, 
-#{parrotname} says #{my_parrot.tweet}, 
-and #{chincillaname} says #{my_chincilla.squeek}."
+puts "#{ferret_name} says #{my_ferret.squeal}, 
+#{parrot_name} says #{my_parrot.tweet}, 
+and #{chinchilla_name} says #{my_chinchilla.squeek}."
  
 puts my_ferret.inspect
 puts my_parrot.inspect
-puts my_chincilla.inspect
+puts my_chinchilla.inspect
